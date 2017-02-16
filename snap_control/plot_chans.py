c6a33bded81b7faa5bc93c7296e02c0b56c5b907
Danny Price <dan@thetelegraphic.com>
2017-02-15 18:22:44 -0800

    Refactor to only plot chans, not deskew too.

--- snap_control/plot_chans.py
+++ snap_control/plot_chans.py
@@ -3,6 +3,51 @@ import logging
 
 import snap
 
+
+def demux_data(snapshot, demux):
+    """ Demux and interleaves data for plotting """
+    input1_data = []
+    input2_data = []
+    input3_data = []
+    input4_data = []
+
+    if demux == 1:
+        for i in range(0, 1024, 4):
+            input1_data.append(snapshot[i + 0])
+            input2_data.append(snapshot[i + 1])
+            input3_data.append(snapshot[i + 2])
+            input4_data.append(snapshot[i + 3])
+            return input1_data, input2_data, input3_data, input4_data
+
+    elif demux == 2:
+        for i in range(0, 1024, 8):
+            input1_data.append(snapshot[i + 0])
+            input1_data.append(snapshot[i + 4])
+            input1_data.append(snapshot[i + 1])
+            input1_data.append(snapshot[i + 5])
+
+            input3_data.append(snapshot[i + 2])
+            input3_data.append(snapshot[i + 6])
+            input3_data.append(snapshot[i + 3])
+            input3_data.append(snapshot[i + 7])
+            return input1_data, input3_data
+
+    elif demux == 4:
+        for i in range(0, 1024, 8):
+            input1_data.append(snapshot[i + 0])
+            input1_data.append(snapshot[i + 2])
+            input1_data.append(snapshot[i + 4])
+            input1_data.append(snapshot[i + 6])
+            input1_data.append(snapshot[i + 1])
+            input1_data.append(snapshot[i + 3])
+            input1_data.append(snapshot[i + 5])
+            input1_data.append(snapshot[i + 7])
+            return input1_data
+
+    else:
+        raise RuntimeError("Weird demux factor, use 1, 2 or 4.")
+
+
 def cmd_tool(args=None):
     from argparse import ArgumentParser
 
@@ -29,105 +74,43 @@ def cmd_tool(args=None):
 
     for chip, chip_num in chip_dict.iteritems():
 
-        # calibrate the snap_control chips using test patterns
-        # s.adc.set_demux_fpga(1)
         snapshot = s.adc.read_ram('adc16_wb_ram{0}'.format(chip_num))
-        #    for i in snapshot:
-        #        print i
-        snapshot = snapshot.tolist()
-        input1_data = []
-        input2_data = []
-        input3_data = []
-        input4_data = []
-        i = 0
 
-        if args.demux_mode == 2:
-            s.enable_pattern('deskew')
-            snapshot = s.adc.read_ram('adc16_wb_ram{0}'.format(chip_num))
-            plt.subplot(3, 3, 1 + chip_num)
-            plt.title('Test Pattern chip %s' % chip)
-            plt.ylim([0, 50])
-            plt.plot(snapshot)
-            s.adc.write(0x25, 0x00)
-            s.adc.write(0x45, 0x00)
-            snapshot = s.adc.read_ram('adc16_wb_ram{0}'.format(chip_num))
-            while i < 1024:
-                input1_data.append(snapshot[i])
-                input1_data.append(snapshot[i + 4])
-                input1_data.append(snapshot[i + 1])
-                input1_data.append(snapshot[i + 5])
-
-                input3_data.append(snapshot[i + 2])
-                input3_data.append(snapshot[i + 6])
-                input3_data.append(snapshot[i + 3])
-                input3_data.append(snapshot[i + 7])
-                i += 8
-            plt.subplot(3, 3, 4 + chip_num)
+        if args.demux_mode == 1:
+            d1, d2, d3, d4 = demux_data(snapshot, args.demux_mode)
+            plt.subplot(4, 3, 1 + chip_num)
             plt.ylim([-40, 40])
-            plt.plot(input1_data)
+            plt.plot(d1, c='#cc00cc')
             plt.title('Input 1 data chip %s' % chip)
-            plt.subplot(3, 3, 7 + chip_num)
+            plt.subplot(4, 3, 2 + chip_num)
             plt.ylim([-40, 40])
-            plt.plot(input3_data)
+            plt.plot(d2, c='#00cccc')
+            plt.title('Input 2 data chip %s' % chip)
+            plt.subplot(4, 3, 3 + chip_num)
+            plt.ylim([-40, 40])
+            plt.plot(d3, c='#cccc00')
+            plt.title('Input 3 data chip %s' % chip)
+            plt.subplot(4, 3, 4 + chip_num)
+            plt.ylim([-40, 40])
+            plt.plot(d4, c='#cc0000')
+            plt.title('Input 4 data chip %s' % chip)
+
+        elif args.demux_mode == 2:
+            d1, d3 = demux_data(snapshot, args.demux_mode)
+            plt.subplot(2, 3, 1 + chip_num)
+            plt.ylim([-40, 40])
+            plt.plot(d1)
+            plt.title('Input 1 data chip %s' % chip)
+            plt.subplot(2, 3, 2 + chip_num)
+            plt.ylim([-40, 40])
+            plt.plot(d3)
             plt.title('Input 3 data chip %s' % chip)
 
-        elif args.demux_mode == 1:
-            s.adc.enable_pattern('deskew')
-            snapshot = s.adc.read_ram('adc16_wb_ram{0}'.format(chip_num))
-            plt.subplot(5, 3, 1 + chip_num)
-            plt.ylim([0, 50])
-            plt.title('Test Pattern chip %s' % chip)
-            plt.plot(snapshot)
-            s.adc.write(0x25, 0x00)
-            s.adc.write(0x45, 0x00)
-            snapshot = s.adc.read_ram('adc16_wb_ram{0}'.format(chip_num))
-            while i < 1024:
-                input1_data.append(snapshot[i])
-                input2_data.append(snapshot[i + 1])
-                input3_data.append(snapshot[i + 2])
-                input4_data.append(snapshot[i + 3])
-
-                i += 4
-            plt.subplot(5, 3, 4 + chip_num)
-            plt.ylim([-40, 40])
-            plt.plot(input1_data)
-            plt.title('Input 1 data')
-            plt.subplot(5, 3, 7 + chip_num)
-            plt.ylim([-40, 40])
-            plt.plot(input2_data)
-            plt.title('Input 2 data')
-            plt.subplot(5, 3, 10 + chip_num)
-            plt.ylim([-40, 40])
-            plt.plot(input3_data)
-            plt.title('Input 3 data')
-            plt.subplot(5, 3, 13 + chip_num)
-            plt.ylim([-40, 40])
-            plt.plot(input4_data)
-            plt.title('Input 4 data')
-
         elif args.demux_mode == 4:
-            s.adc.enable_pattern('deskew')
-            snapshot = s.adc.read_ram('adc16_wb_ram{0}'.format(chip_num))
-            plt.subplot(2, 3, 1 + chip_num)
-            plt.ylim([0, 50])
-            plt.title('Test Pattern chip %s' % chip)
-            plt.plot(snapshot)
-            s.adc.write(0x25, 0x00)
-            s.adc.write(0x45, 0x00)
-            snapshot = s.adc.read_ram('adc16_wb_ram{0}'.format(chip_num))
-            while i < 1024:
-                input1_data.append(snapshot[i])
-                input1_data.append(snapshot[i + 2])
-                input1_data.append(snapshot[i + 4])
-                input1_data.append(snapshot[i + 6])
-                input1_data.append(snapshot[i + 1])
-                input1_data.append(snapshot[i + 3])
-                input1_data.append(snapshot[i + 5])
-                input1_data.append(snapshot[i + 7])
-                i += 8
-            plt.subplot(2, 3, 4 + chip_num)
+            d1 = demux_data(snapshot, args.demux_mode)
+            plt.subplot(1, 3, 2 + chip_num)
             plt.ylim([-6, 6])
-            plt.plot(input1_data)
+            plt.plot(d1)
             plt.title('Input 1 data chip %s' % chip)
         else:
             print('Improper demux mode selected, possible values are 1, 2 and 4')
@@ -135,6 +118,8 @@ def cmd_tool(args=None):
     plt.ylim([-6, 6])
     plt.tight_layout()
     plt.show()
+
+
 
 if __name__ == '__main__':
     cmd_tool()
